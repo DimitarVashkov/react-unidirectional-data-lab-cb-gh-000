1d1ab04f43d48d5e3cdac4797d901f0337c60dad
Dimitar Vashkov <vashkov.dimitar@gmail.com>
2018-04-19 00:16:42 +0100

    Done

--- stores/fileStore.js
+++ stores/fileStore.js
@@ -2,9 +2,36 @@
 
 import Store from './Store';
 
-class FileStore {
+class FileStore extends Store {
+    constructor(initialState){
+        super(initialState);
+        this.state = initialState;
+    }
+
+    removeFile(index){
+        
+        if (this.state.length == 1){
+            this.setState(['']);
+        } else if (this.state.length > 1){
+            this.state.splice(index, 1);
+            this.setState(this.state);
+        }
+    }
+
+    updateFile(index, file) {
+        const files = this.getState().slice();
+        files.splice(index, 1, file);
+        this.setState(files);
+    }
+
+
+    addFile(){
+        this.setState(
+            [...this.state,'']
+        )
+    }
 }
 
-const fileStore = new FileStore([]);
+const fileStore = new FileStore(['']);
 
 export default fileStore;
